69e440cc2230f40e41ad53f96e6128fa12072a82
Thoma Bigueres <t.bigueres@gmail.com>
2017-07-16 18:57:55 +0200

    Modified code to show prediction and display error

--- demo.py
+++ demo.py
@@ -1,15 +1,29 @@
 import pandas as pd
 from sklearn import linear_model
 import matplotlib.pyplot as plt
+import numpy.random as random
 
 #read data
-dataframe = pd.read_fwf('brain_body.txt')
-x_values = dataframe[['Brain']]
-y_values = dataframe[['Body']]
+#Use read_table instead of read_fwf because challenge data is set with , not tabs
+dataframe = pd.read_table('challenge_dataset.txt', ',') 
+x_values = dataframe[['x']]#added header
+y_values = dataframe[['y']]#added header
 
 #train model on data
 body_reg = linear_model.LinearRegression()
 body_reg.fit(x_values, y_values)
+
+
+
+#choose a random line contained in the dataset
+line_to_test = random.randint(0, dataframe.shape[0])
+#predict Y for random X
+predicted_value = body_reg.predict(dataframe.iloc[line_to_test]['x'])
+true_value = dataframe.iloc[line_to_test]['y']
+print('Predicted value : ' + str(predicted_value))
+print("True value : " + str(true_value))
+print("Difference : " + str(predicted_value - true_value))
+
 
 #visualize results
 plt.scatter(x_values, y_values)
